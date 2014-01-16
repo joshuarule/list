eb37cede5ccc3f5cadb80b6087aab60a004f763e
joshuarule <joshua@joshuarule.com>
2014-01-16 00:27:50 -0500

    working on js

---
+++ js/app.js
@@ -0,0 +1,60 @@
+var items, item, name;
+
+$(document).ready(function(){
+
+  items = [];
+
+  name = $("#itemName");
+
+  // click on input select all
+
+  $('.item input[type="text"]').click(function(){
+    var input = this;
+    input.focus();
+    input.setSelectionRange(0,999); 
+  });
+
+  // adds item
+  
+  function addItem() {
+    $('.items').prepend(
+      "<li class='item disabled'>" +
+        "<div class='checkbox'>" +
+          "<input type='checkbox' id='item-" + items.length + "'>" + 
+          "<label for='item-" + items.length + "'></label>" + 
+        "</div>" + 
+        "<input id='itemName' type='text' value='item " + items.length + "'>" + 
+      "</li>");
+
+    return {name: "", disable: true};
+  }
+
+  function deleteItem() {
+    // var index = items.indexOf(itemName.val()) // grab the current index by name
+    // items.splice(index, 1) // remove that index
+  }
+
+  // on click add item 
+
+  $('.add').click(function() {
+    items.push(addItem());
+  });
+
+  // on change update
+
+  $("#itemName").change(function(){
+    alert("change");
+  });
+
+});
+
+// disable item
+
+  // move to bottom
+
+// activate item
+
+// update itemName
+
+// if name has no value then deleteItem
+
